dbd2d42924e420c27ecaa87bef7747d11736b66f
Tim Rourke <tim@timrourke.com>
2015-05-30 17:42:41 -0500

    Adding basic getWinner functionality. Todo: test for valid inputs and return errors.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -37,6 +37,37 @@ function getComputerMove(move) {
 
 function getWinner(playerMove,computerMove) {
     var winner;
+
+    if ( playerMove === computerMove ) {
+
+        winner = "tie";
+
+    } else if ( playerMove === "rock" ) {
+
+        if ( computerMove === "scissors" ) {
+            winner = "player";
+        } else {
+            winner = "computer";
+        }
+        
+    } else if ( playerMove === "paper" ) {
+
+        if ( computerMove === "rock" ) {
+            winner = "player";
+        } else {
+            winner = "computer";
+        }
+
+    } else if ( playerMove === "scissors" ) {
+
+        if ( computerMove === "paper" ) {
+            winner = "player";
+        } else {
+            winner = "computer";
+        }
+
+    }
+    
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
